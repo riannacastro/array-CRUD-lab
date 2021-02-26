e40ef2786e85f53cf19ac2578c5a858c6b44890f
Rianna Castro <riannaxcastro@gmail.com>
2021-02-26 09:33:17 -1000

    commit tree f04f196498ce62a26e92b71bb289fbf2c44276e5

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,43 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  animals = ["cat", "dog", "bird", "fish"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+    animals = ["cat", "dog", "bird", "fish"]
+    animals << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+    animals = ["cat", "dog", "bird", "fish"]
+    animals.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  animals = ["wow", "cat", "dog", "bird", "fish", "arrays!"]
+  animals.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+    animals = ["wow", "cat", "dog", "bird", "fish"]
+    animals.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+    animals = ["cat", "dog", "am", "fish"]
+    animals[2]
+
 end
 
 def retrieve_first_element_from_array(array)
-  
+    animals = ["wow", "cat", "dog", "bird", "fish"]
+    animals[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+    animals = ["wow", "cat", "dog", "bird", "fish", "arrays!"]
+    animals[-1]
 end
